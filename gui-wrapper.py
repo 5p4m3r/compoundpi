522231e4cd1baa5f7891ded50d87639e75ef2d7f
Jan Fabry <jan.fabry@monkeyman.be>
2019-04-04 14:48:02 +0200

    Handle errors in GUI list, sort list

---
+++ gui-wrapper.py
@@ -0,0 +1,10 @@
+__requires__ = 'compoundpi'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('compoundpi', 'gui_scripts', 'cpigui')()
+    )
